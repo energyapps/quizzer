35cb80de60723683c3cc3d891afbfdd722924c88
Daniel Wood <daniel.j.wood11@gmail.com>
2016-03-24 12:19:32 -0400

    remove warning about jekyll

--- makefile.sh
+++ makefile.sh
@@ -21,60 +21,6 @@ do
     fi
 done
 
-# #before we do anything, we make sure that there is a xlsx argument after the shell call
-# if [ "$1" == "$EMPTY" ]; then
-# 	echo "You forgot to include the name of your xlsx file"
-# 	exit
-# elif [ "$TYPE" != "xlsx" ]; then	
-# 	echo $TYPE
-# 	echo "You didn't include the correct type of file. Please use a .xlsx file"
-# 	exit
-# fi
-
-#check to see if brew installed 
-echo "Welcome to the energy.gov quiz generator."
-echo 
-echo
-echo
-echo "You may need your admin password handy!"
-echo
-echo 
-echo
-echo "First we're checking if you have Jekyll running"
-echo
-echo
-echo
-
-
-JEK="$(which jekyll)"
-
-#check if they even have jekyll, because jekyll build --watch can't keep up with the script.
-if [ "$JEK" != "$EMPTY" ]; then
-	echo "Do you have a jekyll server running locally? We're primarily concerned with 'jekyll serve' and 'jekyll build --watch'."
-	echo
-	echo
-	echo
-	echo "Type yes or no. It is case sensitive so type it right!!! (IF YOU DON'T KNOW WHAT THAT IS, YOU DON'T HAVE IT)"
-	read ANSWER
-	# Tell them to kill if yes
-	if [ "$ANSWER" == "yes" ]; then
-		echo "You need kill jekyll servers before running the DOE quiz generator again."
-		exit
-	# proceed if not!
-	elif [ "$ANSWER" == "no" ]; then
-		echo "You may proceed!"
-	else 
-		echo "You typed something wrong, run the script again and answer 'Yes' or 'No' or else..."
-		say "come on my friend, run the script again and follow the directions. Next time I will be angry. jk. l o l. but really, run the script again and type in Yes or No."
-		exit
-	fi	
-else
-	echo "You don't have jekyll, so no worries! You may need that later tho..."
-fi
-
-#Install csvkit stuff
-# ./assets/scripts/installation.sh
-
 #Make the date a variable
 TODAY=$(date +%y-%m-%d)
 echo "Today's Date is $TODAY"
